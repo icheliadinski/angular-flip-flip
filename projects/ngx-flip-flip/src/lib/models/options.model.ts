bdaeef1a83731a9dd2637965a87761915ee3e698
Igor Cheliadinski <igorexec94@gmail.com>
2018-12-14 21:43:34 +0300

    refactor events service

--- projects/ngx-flip-flip/src/lib/models/options.model.ts
+++ projects/ngx-flip-flip/src/lib/models/options.model.ts
@@ -2,4 +2,5 @@ export interface NgxFlipFlipOptions {
   scrollingSpeed: number;
   fitToSectionDelay: number;
   easing: 'linear' | 'ease' | 'ease-in' | 'ease-out' | 'ease-in-out' | 'step-start' | 'step-end';
+  startFromSlide: number;
 }
